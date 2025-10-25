35a17d0cfcb3fd308e8cdad0fa40655b9b031cf0
uselessparkswing <uselessparkswing025@gmail.com>
2025-10-24 18:20:41 -0700

    Started block spawning mechanic for racing level

--- Assets/RacingGameController.cs
+++ Assets/RacingGameController.cs
@@ -1,4 +1,4 @@
-using JetBrains.Annotations;
+using System.Collections;
 using UnityEngine;
 
 public class RacingGameController : MonoBehaviour
@@ -6,17 +6,21 @@ public class RacingGameController : MonoBehaviour
     private int lapsCompleted;
     private int lapsToFinish = 3;
     [SerializeField] private KartController kart;
+    [SerializeField] private GameObject[] blockSpawnPoints;
+    [SerializeField] private GameObject[] blockPrefabs;
+    private int currentBlockIndex = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        lapsCompleted = 0;
+        spawnBlockAtRandomPositionInSpawnPointCollider(blockPrefabs[currentBlockIndex], blockSpawnPoints[0]);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     public void finishLap()
@@ -29,7 +33,21 @@ public class RacingGameController : MonoBehaviour
         }
     }
 
-    public int getLapsCompleted() {
+    public int getLapsCompleted()
+    {
         return lapsCompleted;
-    } 
+    }
+
+    private void spawnBlockAtRandomPositionInSpawnPointCollider(GameObject blockPrefab, GameObject spawnPoint)
+    {
+        Collider spawnCollider = spawnPoint.GetComponent<Collider>();
+        Bounds bounds = spawnCollider.bounds;
+
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+        float z = Random.Range(bounds.min.z, bounds.max.z);
+
+        Vector3 spawnPosition = new Vector3(x, y, z);
+        Destroy(Instantiate(blockPrefab, spawnPosition, spawnPoint.transform.rotation), 10f);
+    }
 }
