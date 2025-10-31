d97fa31537ea5b390bd2e3c1c976bfab6bd3255b
ANDREW-LAPTOP\andre <andrewhyun@live.com>
2025-10-30 17:56:18 -0700

    Started menu

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPressPlay()
+    {
+        //Load the 1st cutscene
+        SceneManager.LoadScene(1);
+    }
+}
