1fda3ab08deec06d726f360d847208d73c66b5a6
ANDREW-LAPTOP\andre <andrewhyun@live.com>
2026-01-18 20:35:02 -0800

    Added some visual/sound FX

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,12 +1,22 @@
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
     public Animator transition;
     public float transitionTime = 1f;
+    public float firstTTime = 4f;
 
+    [SerializeField] GameObject Player;
+    [SerializeField] Transform lerpTrans;
+
+
+    public void Start()
+    {
+        MusicManager.Instance.PlayNewTrack(2);
+    }
     // Update is called once per frame
     void Update()
     {
@@ -15,6 +25,33 @@ public class LevelLoader : MonoBehaviour
 
     public IEnumerator LoadArcadeGame(int levelIndex)
     {
+        Player.GetComponent<PlayableDirector>().Play();
+        SFXManager.Instance.PlaySFX(8);
+        GameObject.Find("GlowThingy").GetComponent<ParticleSystem>().Play();
+
+        Vector3 startPos = Player.transform.position;
+        float elapsed = 0f;
+        float duration = 1f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+
+            float newX = Mathf.Lerp(startPos.x, lerpTrans.position.x, elapsed / duration);
+            Player.transform.position = new Vector3(newX, startPos.y, startPos.z);
+
+            yield return null;
+        }
+
+        // Snap exactly to target X at the end
+        Player.transform.position = new Vector3(
+            lerpTrans.position.x,
+            startPos.y,
+            startPos.z
+        );
+
+        yield return new WaitForSeconds(firstTTime);
+        
         transition.SetTrigger("Start");
 
         yield return new WaitForSeconds(transitionTime);
