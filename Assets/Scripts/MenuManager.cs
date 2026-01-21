71931dd1b5153b80cf387e18704451cad09e49b6
ANDREW-LAPTOP\andre <andrewhyun@live.com>
2026-01-20 21:44:24 -0800

    MenuManager dontdestroy

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -11,13 +11,13 @@ public class MenuManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        DontDestroyOnLoad(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
+        if(Input.GetKeyDown(KeyCode.Return) && SceneManager.GetActiveScene().buildIndex == 0)
         {
             OnPressPlay();
         }
